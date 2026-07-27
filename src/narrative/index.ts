d590ded6142aff77491fe4af379bb4f92a049696
BigBirdReturns <bigbirdreturns@proton.me>
2026-07-27 10:35:17 -0700

    Expose the accepted-action narrative seam

--- src/narrative/index.ts
+++ src/narrative/index.ts
@@ -11,4 +11,5 @@ export * from "./fingerprint.js";
 export * from "./recipe-validate.js";
 export * from "./audit.js";
 export * from "./agency-types.js";
+export * from "./action-receipt-seam.js";
 export * as narrativeSuccession from "./succession/index.js";

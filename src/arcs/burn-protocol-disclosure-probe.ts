96340b59798b7c3adf53b62631a5baeca4a61005
BigBirdReturns <bigbirdreturns@proton.me>
2026-08-01 12:20:45 -0700

    Compile the calibrated Burn publication source

--- src/arcs/burn-protocol-disclosure-probe.ts
+++ src/arcs/burn-protocol-disclosure-probe.ts
@@ -1,6 +1,8 @@
 import { compileCommonShipPocket } from "../common-ship/compiler.js";
-import { BURN_PROTOCOL_DISCLOSURE_PROBE_SOURCE } from "../common-ship/burn-protocol-disclosure-probe.js";
+import {
+  BURN_PROTOCOL_DISCLOSURE_PROBE_PUBLISHED_SOURCE,
+} from "../common-ship/burn-protocol-disclosure-publication.js";
 
 export const BURN_PROTOCOL_DISCLOSURE_PROBE = compileCommonShipPocket(
-  BURN_PROTOCOL_DISCLOSURE_PROBE_SOURCE,
+  BURN_PROTOCOL_DISCLOSURE_PROBE_PUBLISHED_SOURCE,
 );

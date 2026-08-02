2a180140cf7f7bb1977495f6fe82275f5a63aa05
BigBirdReturns <bigbirdreturns@proton.me>
2026-08-01 23:16:34 -0700

    Export reusable Burn chapter assembly and Chapter 2

--- src/burn-protocol/index.ts
+++ src/burn-protocol/index.ts
@@ -1,4 +1,6 @@
 export * from "./types.js";
 export * from "./schema.js";
 export * from "./compiler.js";
+export * from "./assembly.js";
 export * from "./chapter-1.js";
+export * from "./chapter-2.js";
